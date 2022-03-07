28e9bd67ef6fa12991db2079c1b64ebc89e3edd5
Winston T <54168818+wtsia@users.noreply.github.com>
2022-03-07 01:20:27 -0800

    making binary translator

---
+++ binary_translator.py
@@ -0,0 +1,11 @@
+# Converts binary into number
+
+# Choose whether to convert binary into number or vice versa
+
+print('Convert binary into number (1) or number into binary (0)')
+
+
+# convert binary function
+
+
+# convert number function
---
+++ python.py
@@ -0,0 +1,12 @@
+welcome_message = "Initializing of hello Client"
+print(welcome_message)
+
+def function():
+    welcome_message = 'Client Initialized after running through function'
+    print(welcome_message)
+
+function()
+
+print(welcome_message.title())
+
+test = 'nothing'
