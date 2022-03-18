f0ca1ef851f6cb9e4b7e3aa445dd9edb9396d064
Winston T <54168818+wtsia@users.noreply.github.com>
2022-03-18 01:57:40 -0700

    using while, break, and continue

--- python.py
+++ python.py
@@ -1,3 +1,4 @@
+# basics
 welcome_message = "Initializing of hello Client"
 print(welcome_message)
 
@@ -10,3 +11,22 @@ function()
 print(welcome_message.title())
 
 test = 'nothing'
+
+# while and break
+shutoffCommand = 'exit'
+target = 'target'
+
+while True:
+    userInput = input('enter a target guess with similar length to target: ')
+    if userInput == shutoffCommand:
+        break
+    elif userInput < len(target):
+        userInputDiff = len(target) - len(userInput)
+        print('answer is ' + userInputDiff + ' less than target')
+        continue
+    elif userInput > len(target):
+        userInputDiff = len(userInput) - len(target)
+        print('answer is ' + userInputDiff + ' more than target')
+        continue
+    print('length of string is', len(userInput))
+print('Task completed')
