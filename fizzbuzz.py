f52c5bb450178bd8dc6d96fa83629d18c2b5d444
Winston T <54168818+wtsia@users.noreply.github.com>
2022-03-21 01:45:04 -0700

    working on fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,34 @@
 # Fizzbuzz: iterate from 1 to 100, and for every 3rd number, print out 'fizzbuzz'
 # design: get an array of integers from 1 to 100, for loop through every number and if divisible by 3, print Fizzbuzz, otherwise print the number
 
-# for i in range(1, 100):
-#     if i % 3 == 0:
-#         print("Fizzbuzz")
-#     else:
-#         print(i)
-# else:
-#     print("the result is done")
+for i in range(1, 100):
+    if i % 3 == 0:
+        print("Fizzbuzz")
+    else:
+        print(i)
+else:
+    print("the result is done")
 
 # new challenge: make it able to choose a 3rd number from any array of numbers
 # design: iterate through an array and print numbers, but when the index of the array is divisible by 3, print fizzbuzz
 
 userInput = [1, 4, 5, 6, 7, 8, 9, 0]
 print(userInput[3])
+
+def fizzbuzzParser(array):
+    i = 0
+    print('index begins at ', i)
+    for i in array:
+        if i % 3 == 0:
+            print("Fizzbuzz")
+            i += 1
+            print('index is ', i)
+        else:
+            print(array[i])
+            i += 1
+            print('index is ', i)
+    else:
+        print("task completed")
+
+print('running fizzbuzz')
+print(fizzbuzzParser(userInput))
