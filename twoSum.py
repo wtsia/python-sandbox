392c63520184ec118783250ae70926477f14a58c
Winston T <54168818+wtsia@users.noreply.github.com>
2022-06-24 01:05:59 -0700

    added iteration through sequence exercise

--- twoSum.py
+++ twoSum.py
@@ -1,4 +1,3 @@
-###
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # You can return the answer in any order.
@@ -6,7 +5,12 @@
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return
-###
+
+# Drawing Board: take array input, check it is an array, take in target, check it is an int. Assume one solution exists
+# Method 1, Brute Force: Check how the sum of the two integers add up with each successive two terms, i.e. add nums[0] + nums[1] == target, if false, nums[0] + nums[2], ...
+# This method would use recursion to achieve its goal
+# Method 2:
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
